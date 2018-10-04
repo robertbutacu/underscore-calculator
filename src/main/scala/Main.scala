d942d46b14a774ca178c1dad00b3837ea8f5faa8
robertbutacu <robertbutacu@gmail.com>
2018-10-04 14:29:13 +0100

    eval with Either improvements on prettyPrinter

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
-import data.{Add, Multiply}
+import data.{Add, Divide, Multiply, Subtract}
 
 object Main extends App {
-  println(Add(Multiply(1.0, 2.0), Multiply(3.0, 4.0)).prettyPrinter())
+  println(Add(Add(Multiply(1.0, 2.0), Subtract(3.0, 4.0)), Divide(3.0, 1.0)).eval())
   println(Add(3.0, 4.0).prettyPrinter())
 }
--- src/main/scala/data/Calculation.scala
+++ src/main/scala/data/Calculation.scala
@@ -1,27 +1,81 @@
 package data
 
+import scala.util.Try
+
 sealed trait Calculation[A] {
-  def eval()(implicit n: Fractional[A]): A = {
+  def eval()(implicit n: Fractional[A]): Either[String, A] = {
+    def applyOp(a: A, b: A, op: (A, A) => A) = op(a, b)
+
     this match {
-      case Add(a, b)      => n.plus(a.eval(), b.eval())
-      case Subtract(a, b) => n.minus(a.eval(), b.eval())
-      case Multiply(a, b) => n.times(a.eval(), b.eval())
-      case Divide(a, b)   => n.div(a.eval(), b.eval())
-      case Self(v)        => v
+      case Add(a, b) => for {
+        r1 <- a.eval()
+        r2 <- b.eval()
+      } yield applyOp(r1, r2, n.plus)
+      case Subtract(a, b) => for {
+        r1 <- a.eval()
+        r2 <- b.eval()
+      } yield applyOp(r1, r2, n.minus)
+      case Multiply(a, b) => for {
+        r1 <- a.eval()
+        r2 <- b.eval()
+      } yield applyOp(r1, r2, n.times)
+      case Divide(a, b) =>
+        val bEvaluated = b.eval()
+
+        if(bEvaluated.exists(v => n.equiv(n.zero, v)))
+          Left("Division by zero!")
+        else for {
+          r1 <- a.eval()
+          r2 <- bEvaluated
+        } yield applyOp(r1, r2, n.div)
+      case Self(v) => Right(v)
     }
   }
 
   def prettyPrinter(): String = {
-    this match {
-      case Add(s: Self[A], b: Self[A]) => s"""${s.prettyPrinter()} + ${b.prettyPrinter()}"""
-      case Add(s: Self[A], b) => s"""${s.prettyPrinter()} + ( ${b.prettyPrinter()} ) """
-      case Add(s, b: Self[A]) => s""" ( ${s.prettyPrinter()} ) + ${b.prettyPrinter()}"""
-      case Add(a, b) => s""" ( ${a.prettyPrinter()} ) + ( ${b.prettyPrinter()}) """
-      case Subtract(a, b) => s"""( ${a.prettyPrinter()} ) - ( ${b.prettyPrinter()} ) """"
-      case Multiply(a, b) => s"""${a.prettyPrinter()} * ${b.prettyPrinter()}"""
-      case Divide(a, b) => s"""${a.prettyPrinter()} / ${b.prettyPrinter()}"""
-      case Self(v)      => v.toString
+    val opening: List[String] = List("(", "[", "{")
+    val closing: List[String] = List(")", "]", "}")
+
+    def getDepthLevelBracesOpening(level: Int): String = Try {
+      opening(level)
+    }.getOrElse(opening.last)
+
+    def getDepthLevelBracesClosing(level: Int): String = Try {
+      closing(level)
+    }.getOrElse(closing.last)
+
+    def getDepth(curr: Calculation[A]): Int = {
+      curr match {
+        case Add(a, b) => 1 + Math.max(getDepth(a), getDepth(b))
+        case Subtract(a, b) => 1 + Math.max(getDepth(a), getDepth(b))
+        case Multiply(a, b) => Math.max(getDepth(a), getDepth(b))
+        case Divide(a, b) => Math.max(getDepth(a), getDepth(b))
+        case Self(v) => 0
+      }
     }
+
+    val maxDepth = getDepth(this) - 1
+
+    def go(curr: Calculation[A], depthLevel: Int = 1): String = {
+      val opening = getDepthLevelBracesOpening(Math.min(2, maxDepth - depthLevel))
+      val closing = getDepthLevelBracesClosing(Math.min(2, maxDepth - depthLevel))
+
+      curr match {
+        case Add(s: Self[A], b: Self[A]) => s"""${go(s)} + ${go(b)}"""
+        case Add(s: Self[A], b) => s"""${go(s)} + $opening ${go(b, depthLevel + 1)} $closing """
+        case Add(s, b: Self[A]) => s""" $opening ${go(s, depthLevel + 1)} $closing + ${b.prettyPrinter()}"""
+        case Add(a, b) => s""" $opening ${go(a, depthLevel + 1)} $closing + $opening ${go(b, depthLevel + 1)} $closing """
+        case Subtract(s: Self[A], b: Self[A]) => s"""${go(s)} - ${go(b)}"""
+        case Subtract(s: Self[A], b) => s"""${go(s)} - $opening ${go(b, depthLevel + 1)} $closing """
+        case Subtract(s, b: Self[A]) => s""" $opening ${go(s, depthLevel + 1)} $closing - ${b.prettyPrinter()}"""
+        case Subtract(a, b) => s"""$opening ${go(a, depthLevel + 1)} $closing - $opening ${go(b, depthLevel + 1)} $closing """"
+        case Multiply(a, b) => s"""${go(a, depthLevel + 1)} * ${go(b, depthLevel + 1)}"""
+        case Divide(a, b) => s"""${go(a, depthLevel + 1)} / ${go(b, depthLevel + 1)}"""
+        case Self(v) => v.toString
+      }
+    }
+
+    go(this)
   }
 }
 
@@ -30,8 +84,11 @@ object Calculation {
 }
 
 case class Add[A](a: Calculation[A], b: Calculation[A]) extends Calculation[A]
+
 case class Subtract[A](a: Calculation[A], b: Calculation[A]) extends Calculation[A]
+
 case class Multiply[A](a: Calculation[A], b: Calculation[A]) extends Calculation[A]
+
 case class Divide[A](a: Calculation[A], b: Calculation[A]) extends Calculation[A]
 
 case class Self[A](value: A) extends Calculation[A]
